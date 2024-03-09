83e32dd4ec112a101a7406cc2f75b1175c58b9ff
Amr Imam <emama2337@gmail.com>
2024-03-09 03:41:16 +0200

    i tired

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import ServicePackage from "@/Components/Home/servicePackage/ServicePackage"
 import BarLine from "@/Components/Home/barLine/BarLine"
 import Info from "@/Components/Home/info/Info"
 import ServiceContent from "@/Components/Home/serviceContent/ServiceContent"
-import CounterPage from "@/Components/Home/counter/CounterPage"
+import CounterPage from "@/Components/Home/counterpage/CounterPage"
 import CompletedProjects from "@/Components/Home/completedProjects/CompletedProjects"
 
 export default function Home() {
