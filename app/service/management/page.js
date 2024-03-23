c4d790897b653fccf26ea2a825e6c264c1529a05
Amr Imam <emama2337@gmail.com>
2024-03-23 19:40:28 +0200

    update

--- app/service/management/page.js
+++ app/service/management/page.js
@@ -1,3 +1,33 @@
+import ServiceDetail from "@/Components/motion/serviceDetails/ServiceDetail"
+import PageHead from "@/Components/utils/PageHead/PageHead"
+import BarLine from "@/Components/Home/barLine/BarLine"
+import BoxService from "@/Components/motion/servicesBoxs/BoxService"
+import MotionProjects from "@/Components/motion/projects-of-motion/MotionProjects"
+
 export default function Management() {
-  return <main>Menagement</main>
+  const linksList = [
+    {
+      id: "01",
+      link: "/",
+      titleAr: "الرئيسية/",
+      titleEn: "",
+    },
+    {
+      id: "02",
+      link: "/",
+      titleAr: "موشن جرافيك",
+      titleEn: "",
+    },
+  ]
+
+  return (
+    <main>
+      <PageHead pageTitle="خدماتنا" links={linksList} />
+      <ServiceDetail />
+      <BarLine />
+      <BoxService />
+      <MotionProjects />
+      <BarLine />
+    </main>
+  )
 }
