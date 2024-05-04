dca7124d9df8f248d66963080a6d28a133b9bc05
Amr Imam <emama2337@gmail.com>
2024-05-04 17:27:37 +0300

    add photo

--- Components/poster/projects-of-motion/MotionProjects.jsx
+++ Components/poster/projects-of-motion/MotionProjects.jsx
@@ -4,82 +4,377 @@ import styles from "./styles.module.scss"
 import Link from "next/link"
 const projects = [
   {
-    image: "",
+    image: "/images/بوستات/1.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/2.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/3.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/4.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/5.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/6.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/7.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/8.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/9.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/10.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/11.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/12.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/13.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/14.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/15.jpg",
     title: "",
     link: "",
   },
   {
-    image: "",
+    image: "/images/بوستات/16.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/17.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/18.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/19.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/20.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/21.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/22.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/23.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/24.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/25.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/26.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/27.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/28.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/29.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/30.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/31.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/32.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/33.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/34.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/35.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/36.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/37.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/38.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/39.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/40.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/41.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/42.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/43.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/44.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/45.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/46.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/47.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/48.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/49.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/50.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/51.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/52.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/53.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/54.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/55.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/56.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/57.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/58.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/59.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/60.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/61.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/62.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/63.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/64.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/65.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/66.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/67.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/68.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/69.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/70.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/80.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/81.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/82.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/83.jpg",
+    title: "",
+    link: "",
+  },
+  {
+    image: "/images/بوستات/84.jpg",
     title: "",
     link: "",
   },
